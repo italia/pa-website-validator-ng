3e6c199f74733e4024a2971c8b0a40d2cf32cb50
luca.carrisi <luca.carrisi@ccenture.com>
2024-09-30 14:44:28 +0200

    updates

--- src/audits/lighthouse/index.ts
+++ src/audits/lighthouse/index.ts
@@ -1,18 +1,23 @@
 
-import crawlerTypes from "../..//types/crawler-types";
 import {Audit} from "../Audit.js";
-import PageData = crawlerTypes.PageData
 
 import lighthouse from 'lighthouse';
+import {Page} from "puppeteer";
 
 class lighthouseAudit extends Audit {
 
-    async auditPage(page:PageData) {
-        const runnerResult = await this.runLighthouse(page.url, {});
-        console.log(runnerResult)
-        //return runnerResult.report;
+    async auditPage(page: Page | null) {
+        if(page){
+            const url = page.url();
+            const runnerResult = await this.runLighthouse(url, {});
+            console.log(runnerResult)
+            //return runnerResult.report;
 
-        return runnerResult;
+            return runnerResult;
+        }
+
+        return;
+
     }
 
     async runLighthouse(url: string, options: any): Promise<any> {
