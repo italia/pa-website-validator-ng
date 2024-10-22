a06db482563f4d4d3668545a7dca00c6306590c0
simone-amadio-acn <simone.amadio@accenture.com>
2024-10-22 16:39:34 +0200

    feat: prettier

--- src/launchScript.ts
+++ src/launchScript.ts
@@ -136,7 +136,7 @@ async function run(
 
     if (browser) {
       console.error("closing puppeteer");
-      await browser.process()?.kill()
+      await browser.process()?.kill();
     }
 
     return finalResults;
